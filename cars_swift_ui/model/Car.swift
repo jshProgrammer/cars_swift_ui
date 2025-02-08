4261ddf94177146c4ed0a662f0ea483f5284d6e4
jshProgrammer <jpfennig2403@gmail.com>
2025-02-08 17:43:19 +0100

    implementing first decodable car model

--- cars_swift_ui/model/Car.swift
+++ cars_swift_ui/model/Car.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+struct Car: Decodable {
+    var brand: String
+    var model: String
+    var horsepower: Int
+    var year: Int
+    var fuelType: String
+    var price: Int
+    var image: String
+    var transmission: Transmission
+    var carType: CarType
+    
+    enum Transmission: String, Decodable { 
+        case automatic = "Automatic"
+        case manual = "Manual"
+    }
+    enum CarType: String, Decodable {
+        case coupe = "Coupe"
+        case suv = "SUV"
+        case sedan = "Sedan"
+        case van = "Van"
+    }
+}
