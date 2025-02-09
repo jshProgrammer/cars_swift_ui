f5e2ccb02943afbb5636383df12e6a2a0b24b7d6
jshProgrammer <jpfennig2403@gmail.com>
2025-02-09 19:11:27 +0100

    inserting real image urls in data.json

---
+++ cars_swift_ui/view/CarCellView.swift
@@ -0,0 +1,18 @@
+//
+//  CarCellView.swift
+//  cars_swift_ui
+//
+//  Created by Joshua Pfennig on 09.02.25.
+//
+
+import SwiftUI
+
+struct CarCellView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CarCellView()
+}
