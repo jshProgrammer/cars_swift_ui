fc99efbb0091644b88c96bf90920ff0cb67aa161
jshProgrammer <jpfennig2403@gmail.com>
2025-02-10 19:28:33 +0100

    adding TabView

---
+++ cars_swift_ui/screen/CarTabBarScreen.swift
@@ -0,0 +1,18 @@
+//
+//  CarTabBarScreen.swift
+//  cars_swift_ui
+//
+//  Created by Joshua Pfennig on 10.02.25.
+//
+
+import SwiftUI
+
+struct CarTabBarScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CarTabBarScreen()
+}
